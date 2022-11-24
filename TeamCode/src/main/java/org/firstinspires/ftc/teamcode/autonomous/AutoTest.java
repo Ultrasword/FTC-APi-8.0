f357d5379bbd0157afca53392d9cb8d95af038d9
vmkcr <vmkcreeper@gmail.com>
2022-11-24 15:02:34 -0500

    updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoTest.java
@@ -4,21 +4,63 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.system.Vision;
 import org.firstinspires.ftc.teamcode.wrappers.Gear;
 import org.firstinspires.ftc.teamcode.wrappers.MotorRatio;
 import org.firstinspires.ftc.teamcode.wrappers.MotorWrapper;
 import org.firstinspires.ftc.teamcode.system.Chassis;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
 @TeleOp(name="susssy bajkakds afmdksa")
 public class AutoTest extends LinearOpMode {
+    Vision sleeveDetection = new Vision();
+    OpenCvCamera camera;
+    String webcamName = "Webcam 1";
+
     @Override
     public void runOpMode() throws InterruptedException {
         Chassis chassis = new Chassis();
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+//        camera = OpenCvCameraFactory.getInstance().createInternalCamera2(OpenCvInternalCamera2.CameraDirection.BACK, cameraMonitorViewId);
+        WebcamName webcamName = hardwareMap.get(WebcamName.class,"camera");
+        //OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName); without live preview
+        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+
+        sleeveDetection = new Vision();
+        camera.setPipeline(sleeveDetection);
+
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(320,240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {}
+        });
+
+        while (!isStarted()) {
+            telemetry.addData("POSITION: ", sleeveDetection.getPosition());
+            telemetry.update();
+        }
 
         waitForStart();
 
         while(opModeIsActive()){
-            chassis.goStraight(6);
+            if(sleeveDetection.getPosition() == Vision.ParkingPosition.LEFT){
+
+            }
+            else if(sleeveDetection.getPosition() == Vision.ParkingPosition.RIGHT){
+
+            }
+            else if(sleeveDetection.getPosition() == Vision.ParkingPosition.CENTER){
+                chassis.goStraight(6);
+            }
             wait(1000);
         }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/OverallSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/OverallSample.java
@@ -72,8 +72,5 @@ public class OverallSample extends LinearOpMode {
             telemetry.update();
             sleep(15);
         }
-
-
-
     }
 }
