f9216d9ad187ee50a15429bea0cc62dea398f913
mynameispet <ultraswordkirbypink@gmail.com>
2022-12-01 14:45:18 -0500

    logging filenaming ez

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/wrappers/LoggingSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/wrappers/LoggingSystem.java
@@ -8,15 +8,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class LoggingSystem {
 
     public static FileWriter fileWriter;
 
     public static void initLogging(){
-
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
         try {
-            fileWriter = new FileWriter(new File("test.txt"));
+            fileWriter = new FileWriter(new File(String.format("%s.txt", dateFormat.format(date))));
         } catch (IOException e) {
             e.printStackTrace();
         }
